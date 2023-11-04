fab00d4cae6de94e44e691ab7de53c5e8fa35c72
Nate Sawant <natesawant@gmail.com>
2023-11-04 16:12:15 -0400

    Add string parsing

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,7 +15,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-  defer file.Close()
+	defer file.Close()
 
 	// Create a command for the C++ project.
 	cmd := exec.Command(
@@ -29,5 +33,65 @@ func main() {
 
 	// Print the output (including any error messages)
 	fmt.Println("C++ module output:")
-	fmt.Println(string(output))
+
+	parseOutput(string(output))
+
+	//fmt.Println(data)
+}
+
+type SliceData struct {
+	Flavor            string  `json:"flavor"`
+	TimeS             int     `json:"time"`
+	FilamentUsed      float32 `json:"filamentused"`
+	LayerHeight       float32 `json:"layerheight"`
+	MinX              float32 `json:"minx"`
+	MinY              float32 `json:"miny"`
+	MinZ              float32 `json:"minz"`
+	MaxX              float32 `json:"maxx"`
+	MaxY              float32 `json:"maxy"`
+	MaxZ              float32 `json:"maxz"`
+	TargetMachineName string  `json:"targetmachinename"`
+}
+
+func parseOutput(output string) SliceData {
+	var rex = regexp.MustCompile(";([^:]+):([^;]+)")
+
+	data := rex.FindAllStringSubmatch(output, -1)
+
+	res := make(map[string]interface{})
+	for _, kv := range data {
+		k := strings.ToLower(regexp.MustCompile(`[^a-zA-Z]+`).ReplaceAllString(kv[1], ""))
+		v := strings.Replace(kv[2], "\n", "", -1)
+		if k != "targetmachinename" && k != "flavor" {
+			v = strings.ReplaceAll(v, " ", "")
+			v = strings.ReplaceAll(v, "m", "")
+			i, err := strconv.ParseFloat(v, 32)
+			if err != nil {
+				// ... handle error
+				panic(err)
+			}
+			res[k] = i
+		} else {
+			res[k] = v
+		}
+	}
+	fmt.Println(res)
+
+	jsonData, jsonErr := json.Marshal(res)
+	if jsonErr != nil {
+		// ... handle error
+		panic(jsonErr)
+	}
+
+	// Convert the JSON to a struct
+	var structData SliceData
+	unmarshalErr := json.Unmarshal(jsonData, &structData)
+	if unmarshalErr != nil {
+		// ... handle error
+		panic(unmarshalErr)
+	}
+
+	fmt.Println(structData)
+
+	return structData
 }
